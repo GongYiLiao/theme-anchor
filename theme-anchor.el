cd6e0213787985a8c395779d898a0c5edd73a9d3
Liāu, Kiong-Gē <gongyi.liao@gmail.com>
2021-03-25 14:57:04 -0500

    change of API, now theme-anchor-spec-inherit is designed for theme-anchor-buffer-local only

--- theme-anchor.el
+++ theme-anchor.el
@@ -30,11 +30,11 @@
 ;; buffer only
 
 ;;; Code:
-(require 'cl-lib)  			;; for 'cl-remove-if'
-(require 'faces) 			;; for 'face-spec-choose'
-(require 'custom) 			;; for 'load-theme'
-(require 'face-remap) 			;; for 'face-remap-set-base'
-(require 'ansi-color)                   ;; for 'ansi-color-make-color-map
+(require 'cl-lib)  			;; for `cl-remove-if', `cl-remove', `cl-set-difference'
+(require 'faces) 			;; for `face-spec-choose'
+(require 'custom) 			;; for `load-theme'
+(require 'face-remap) 			;; for `face-remap-set-base'
+(require 'ansi-color)                   ;; for `ansi-color-make-color-map'
 
 (defun theme-anchor-get-values (theme)
   "Extract all the theme-face values from THEME."
@@ -70,29 +70,29 @@ Argument FACE-SPEC: the specs to be tested"
 	  ;; the applicable face spec chosen by 'face-spec-choose'
 	  (face-spec-choose face-spec-content))))
 
-(defun theme-anchor-set-inherit (&optional buffer)
-  "Get specs from parent faces using face's inherit attribute.
-This function should only apply after the first stage, e.g.,
-face specs defined in the theme have been applied and a
-buffer-local face-remapping-alist has been properly generate." 
-  (with-current-buffer (if buffer buffer (current-buffer))
-    ;; f-keys are the face specs anchored in current buffer
-    ;; via face-remap-set-base
-    (let ((f-keys (mapcar #'car face-remapping-alist)))
-      (mapc (lambda (spec) (if spec (apply #'face-remap-set-base spec)))
-	    ;; This filter check the following conditions 
-	    ;; 1. the face's spec is derived through :inherit property
-	    ;; 2. if #1 holds, check if the face this face inherit
-	    ;;    from does exist in curent buffer's face-remapping-alist
-	    (mapcar (lambda (fc)
-		      (let ((ihr (face-attribute fc :inherit nil t)))
-			(if (and (not (eq ihr 'unspecified))
-				 (member ihr f-keys))
-			    (list fc (car (alist-get ihr
-						     face-remapping-alist))))))
-		    ;; faces obtain from global face lists  whose names are not
-		    ;;  in current buffer's face-remapping-alist 
-		    (cl-set-difference (face-list) f-keys))))))
+;; (valid-specs (cl-remove nil (mapcar #'theme-anchor-spec-choose
+;; 					     (theme-anchor-get-faces theme))))
+
+(defun theme-anchor-spec-inherit (valid-specs)
+  "Get specs using inherit proporties.
+This is for those faces not specified in agiven theme but have their parent
+faces in the theme. This function is designed for `theme-anchor-buffer-local'
+and not supposed to use separately" 
+  (let* ((valid-keys (mapcar #'car valid-specs))
+	 ;; get name of faces those exists but not specified in theme
+	 (face-stack (cl-set-difference (face-list) valid-keys))
+	 ;; This filter check the following conditions 
+	 ;; 1. the face's spec is derived through :inherit property
+	 ;; 2. if #1 holds, check if the face this face inherit
+	 ;;    from does exist in curent buffer's face-remapping-alist
+	 (inherit-specs (mapcar (lambda (fc)
+				  (let ((ihr (face-attribute fc :inherit nil t)))
+				    (if (and (not (eq ihr 'unspecified))
+					     (member ihr valid-keys))
+					(list fc (car (alist-get ihr valid-specs))))))
+				face-stack)))
+    ;; clean up `nil's 
+    (cl-remove nil inherit-specs)))
 
 (defun theme-anchor-buffer-local (theme)
   "Extract applicable face settings from THEME.
@@ -112,11 +112,13 @@ It uses 'face-remap-set-base' to load that theme in a buffer local manner"
   ;; set the theme-values as well 
   (theme-anchor-set-values theme)
   ;; choose the most appropriate theme for the environment 
-  (let ((valid-specs
-	 (remove 'nil ;; filter out non-applicable specs
-		 (mapcar #'theme-anchor-spec-choose
-			 ;; get the theme-face specs from the theme
-			 (theme-anchor-get-faces theme)))))
+  (let* ((valid-specs
+	  (cl-remove 'nil ;; filter out non-applicable specs
+		     (mapcar #'theme-anchor-spec-choose
+			     ;; get the theme-face specs from the theme
+			     (theme-anchor-get-faces theme))))
+	 (inherit-specs (theme-anchor-spec-inherit valid-specs))
+	 (valid-and-inherit-specs (append valid-specs inherit-specs)))
     ;; make sure the face is set as the buffer's based face by
     ;; 1. use face-remap-se-base face `nil' to make global face ignored
     ;; 2. apply the spec as the new base for the buffer 
@@ -125,7 +127,7 @@ It uses 'face-remap-set-base' to load that theme in a buffer local manner"
 	    ;; (face-remap-set-base (car spec) nil)
 	    (apply #'face-remap-set-base spec))	;; anchor the spec as base 
 	  ;; ignore faces without applicable specs
-	  valid-specs)))
+	  valid-and-inherit-specs)))
 
 (defmacro theme-anchor-hook-gen (theme &rest other-step)
   "Generate hook functions.
@@ -135,8 +137,7 @@ Optional argument OTHER-STEP the additional steps to execute in the mode hook."
      ;; face-remap current buffer with theme
      (theme-anchor-buffer-local ,theme)
      ;; other sides effect applicable to the current buffer
-     ,@other-step
-     (theme-anchor-set-inherit)))
+     ,@other-step))
 
 (provide 'theme-anchor)
 ;;; theme-anchor.el ends here
